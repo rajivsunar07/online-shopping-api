4acd1b9498d20dcb2faff5c2ed689506c1bbc7bb
rajivsunar <rajivsunar07@gmail.com>
2021-08-28 12:45:17 +0545

    File Upload middleware updated

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -2,16 +2,16 @@ const multer = require('multer')
 
 // destination to store files
 const storage = multer.diskStorage({
-    destination: function(req,res,cb){
+    destination: function(req, file,cb){
         cb(null, './files')
     },
-    filename: function(req, res, cb){
+    filename: function(req, file, cb){
         cb(null, Date.now() + file.originalname)
     }
 })
 
 const filter = function(req, file, cb){
-    if(file.mimetype == 'image/png'){
+    if(file.mimetype == 'image/png' || file.mimetype == 'image/jpeg' || file.mimetype == 'image/png'){
         cb(null, true)
     }
     else{
