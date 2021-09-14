1687d3ebe98374ac91f6396a54fb1db997ba0188
rajivsunar <rajivsunar07@gmail.com>
2021-09-14 19:59:43 +0545

    Get user data and update user data

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv")
+const fs = require('fs')
 
 const User = require("../models/user")
 
@@ -33,95 +34,119 @@ exports.register = (req, res) => {
 
                         // save the user
                         user
-                        .save()
-                        .then(result => {
-                            // if user succesfully saved
-                            res.status(201).json({
-                                success: true,
-                                message: "User created succesfully"
+                            .save()
+                            .then(result => {
+                                // if user succesfully saved
+                                res.status(201).json({
+                                    success: true,
+                                    message: "User created succesfully"
+                                })
                             })
-                        })
-                        .catch(err => {
-                            // if an error occures
-                            console.log(err)
-                            res.status(500).json({
-                                error: err
+                            .catch(err => {
+                                // if an error occures
+                                console.log(err)
+                                res.status(500).json({
+                                    error: err
+                                })
                             })
-                        })
                     }
                 })
             }
         })
-    };
+};
 
 
 exports.login = (req, res) => {
     User.find({ email: req.body.email })
-    .exec()
-    .then(user => {
-        if(user.length < 1){
-            return res.status(401).json({
-                message: "Authentication failed"
-            })
-        }
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-            if(err){
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
                 return res.status(401).json({
                     message: "Authentication failed"
                 })
             }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: "Authentication failed"
+                    })
+                }
 
-            if(result){
-                const token = jwt.sign(
-                    {
-                        email: user[0].email,
-                        userId: user[0]._id
-                    },
-                    process.env.JWT_SECRET_KEY,
-                    {
-                        expiresIn : '24h'
-                    }
-                )
-                return res.status(200).json({
-                    success: true,
-                    message: "Authentication successful",
-                    token: token
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        process.env.JWT_SECRET_KEY,
+                        {
+                            expiresIn: '24h'
+                        }
+                    )
+                    return res.status(200).json({
+                        success: true,
+                        message: "Authentication successful",
+                        token: token
+                    })
+                }
+                res.status(401).json({
+                    message: "Authentication failed"
                 })
-            }
-            res.status(401).json({
-                message: "Authentication failed"
             })
         })
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
         })
-    })
 }
 
 exports.get_user = (req, res, next) => {
+    res.status(200).json({
+        user: req.userdata 
+    })  
+}
 
-    try{
-        const token = req.headers.authorization.split(" ")[1] // bearer token
-        const data = jwt.verify(token, process.env.JWT_SECRET_KEY)
+exports.get_user_from_id = (req, res, next) => {
 
-        User.findOne({_id: data.userId})
+    User.findOne({ _id: req.params.id })
         .then(result => {
             res.status(200).json({
-                user: result
+                success: true,
+                result: result
             })
             next()
         })
         .catch(err => {
-            res.status(401).json({error: err})
+            res.status(401).json({ error: err })
         })
-    }
-    catch(err){
-        res.status(401).json({
-            error: err,
-            message: "User not found"
-        })
-    }
+
 }
+
+exports.update = (req, res, next) => {
+
+    const id = req.userdata._id;
+    
+    User.findByIdAndUpdate(id, req.body)
+      .exec()
+      .then(result => {
+        User.findByIdAndUpdate(id, {image: req.file.path})
+        .then(result => {
+            res.status(200).json({
+                message: "User details updated"
+              });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "User image update failed"
+            })
+        })
+        
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  };
