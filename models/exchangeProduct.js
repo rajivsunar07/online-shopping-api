c390e044d2d4c2b7f88039585c3f034d5a5c4409
rajivsunar <rajivsunar07@gmail.com>
2021-09-14 20:00:37 +0545

    Model for exchange products

---
+++ models/exchangeProduct.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const exchangeProductSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: [String],
+        required: true
+    },
+    exchangeFor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'product'
+    },
+    accepted: {
+        type: Boolean,
+        default: false
+    }
+},
+    {
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    })
+
+const exchangeProduct = mongoose.model('exchangeProduct', exchangeProductSchema)
+
+module.exports = exchangeProduct
