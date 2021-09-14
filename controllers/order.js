ad1ceecc1f1507847d2c1c9b7a1b380c89eed0e1
rajivsunar <rajivsunar07@gmail.com>
2021-09-14 15:10:57 +0545

    Update quantity and price of order item and total price of order if same product is clicked twice in a order

--- controllers/order.js
+++ controllers/order.js
@@ -66,47 +66,91 @@ exports.get_for_user = (req, res, next) => {
         })
 }
 
-exports.add_order_item = (req, res, next) =>{
-    const orderItem = new OrderItem({
-        _id: mongoose.Types.ObjectId(),
-        product: req.body.product,
-        quantity: req.body.quantity,
-        price: req.body.price,
-        seller: req.body.seller
-    })
+exports.add_order_item = (req, res, next) => {
 
-    orderItem.save()
-    .then(r => {
-        Order.find({ user: req.userdata._id, ordered: false }).sort({ created_at: -1 }).limit(1)
+    Order.find({ user: req.userdata._id, ordered: false }).sort({ created_at: -1 }).limit(1)
+        .populate('item')
         .then(result => {
-            Order.findOneAndUpdate({ _id: result[0]._id}, { total_price: String(parseInt(result[0].total_price) + parseInt(req.body.price)) }, { $addToSet : { item: orderItem}})
-            .exec()
-            .then(rslt => {
-                res.status(200).json({
-                    success: true,
-                    message: "Order item added to the order"
+            console.log(result[0]);
+            let exists = false
+            let item = null
+            let current = result[0]
+            for (i in current.item) {
+                if (current.item[i].product == req.body.product) {
+                    exists = true
+                    item = current.item[i]
+                    break
+                }
+            }
+
+            if (exists) {
+                OrderItem.findByIdAndUpdate(item._id, { price: (item.price / item.quantity) + item.price, quantity: parseInt(item.quantity) + 1 })
+                    .then(r => {
+                        Order.findByIdAndUpdate(result[0]._id, { total_price: String(parseInt(result[0].total_price) + parseInt(r.price))})
+                        .exec()
+
+
+                        res.status(200).json({
+                            success: true,
+                            message: "Order item quantity and price increased"
+                        })
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            error: err,
+                            message: "Order item quantity and price not incrased"
+                        })
+                    })
+
+            } else if (exists == false){
+
+                const orderItem = new OrderItem({
+                    _id: mongoose.Types.ObjectId(),
+                    product: req.body.product,
+                    quantity: req.body.quantity,
+                    price: req.body.price,
+                    seller: req.body.seller
                 })
-            })
-            .catch(err => {
-                res.status(500).json({
-                    error: err,
-                    message: "Order item not added to the order"
-                })
-            })
+
+                orderItem.save()
+                    .then(r => {
+                        Order.find({ user: req.userdata._id, ordered: false }).sort({ created_at: -1 }).limit(1)
+                            .then(result => {
+                                Order.findByIdAndUpdate( result[0]._id , { $addToSet: { item: orderItem }, total_price: String(parseInt(result[0].total_price) + parseInt(req.body.price)) })
+                                    .exec()
+                                    .then(rslt => {
+                                        res.status(200).json({
+                                            success: true,
+                                            message: "Order item added to the order"
+                                        })
+                                    })
+                                    .catch(err => {
+                                        res.status(500).json({
+                                            error: err,
+                                            message: "Order item not added to the order"
+                                        })
+                                    })
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err,
+                                    message: "Order not found"
+                                })
+                            })
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            error: err,
+                            message: "Failed to save order item"
+                        })
+                    })
+            }
         })
-        .catch(err => {
-            res.status(500).json({
-                error: err,
-                message: "Order not found"
-            })
-        })
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err,
-            message: "Failed to save order item"
-        })
-    }) 
+
+
+
+
+
 }
 
 exports.update_order_item = (req, res, next) => {
@@ -129,18 +173,18 @@ exports.delete_order_item = (req, res, next) => {
     OrderItem.findByIdAndDelete(req.params.itemId)
         .then(result => {
             // console.log(result)
-            Order.findOneAndUpdate({ item: result._id }, { $pull: { item:  { _id: result._id }}}, { safe: true })
-            .then(r => {
-                res.status(200).json({
-                    message: "Order item deleted and removed successfully"
+            Order.findOneAndUpdate({ item: result._id }, { $pull: { item: { _id: result._id } } }, { safe: true })
+                .then(r => {
+                    res.status(200).json({
+                        message: "Order item deleted and removed successfully"
+                    })
                 })
-            })
-            .catch(err =>{
-                res.status(500).json({
-                    error: err,
-                    message: "Error in removing item form the order"
+                .catch(err => {
+                    res.status(500).json({
+                        error: err,
+                        message: "Error in removing item form the order"
+                    })
                 })
-            })
         })
         .catch(err => {
             res.status(500).json({
