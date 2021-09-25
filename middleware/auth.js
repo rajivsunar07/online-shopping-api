d25f14377af1e041173d98cb467f3a9b09fca5fa
rajivsunar <rajivsunar07@gmail.com>
2021-09-25 11:53:50 +0545

    VerifyAdmin function added

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken') // used to checck whether user is logged in or not
 const User = require('../models/user')
 
 module.exports.verifyUser = function(req, res, next){
     try{
-        const token = req.headers.authorization.split(" ")[1]
+        const token = req.headers.authorization.split(" ")[1] // bearer token
         const data = jwt.verify(token, process.env.JWT_SECRET_KEY)
 
         User.findOne({_id: data.userId})
@@ -19,6 +19,16 @@ module.exports.verifyUser = function(req, res, next){
         res.status(401).json({
             error: err,
             message: "Authentication failed"
+        })
+    }
+}
+
+module.exports.verifyAdmin = (req, res, next) => {
+    if(req.userdata.is_admin) {
+        next()
+    }else{
+        res.status(401).json({
+            message: "Authorization failed"
         })
     }
 }
