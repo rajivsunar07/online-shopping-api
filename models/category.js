f515f5d8e04dd53bac02c7987043a8d8bb68f0b8
rajivsunar <rajivsunar07@gmail.com>
2021-09-25 16:55:02 +0545

    Category schema created

---
+++ models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const categorySchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    category: {
+        type: String
+    },
+    subCategory: {
+        type: [String]
+    }
+}, {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+})
+
+
+const Category = mongoose.model('Category', categorySchema)
+
+module.exports = Category
